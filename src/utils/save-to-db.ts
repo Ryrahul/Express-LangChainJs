f737b592b367559613ba6978ee71104370d064a9
Rahul <caulirahul6@gmail.com>
2024-04-17 08:26:43 +0530

    fix: fix

--- src/utils/save-to-db.ts
+++ src/utils/save-to-db.ts
@@ -8,16 +8,16 @@
 // import { SupabaseVectorStore } from "@langchain/community/vectorstores/supabase";
 // const sbApiKey: string = process.env.SUPABASE_API_KEY || "";
 // const sbUrl: string = process.env.SUPABASE_API_URL || "";
-// // import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+// import { PDFLoader } from "langchain/document_loaders/fs/pdf";
 
 // const client = createClient(sbUrl, sbApiKey);
 // (async () => {
-//   // const loader = new CSVLoader("./datafile.csv");
-//   // const docs = await loader.load();
+//   //   const loader = new CSVLoader("./datafile.csv");
+//   //   const docs = await loader.load();
 
-//   // const loader = new PDFLoader("./fire.pdf");
+//   const loader = new PDFLoader("./forestfire.pdf");
 
-//   // const docs = await loader.load();
+//   const docs = await loader.load();
 
 //   const openAIApiKey = process.env.OPEN_AI_API_KEY;
 
